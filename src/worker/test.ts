479429f7e98ee4dd94e33356791092abe0460d06
u-user-u <yuki.oka@nsw.co.jp>
2024-07-24 16:43:44 +0900

    5-3 Web Worker でマークダウンの変換処理を行う

--- src/worker/test.ts
+++
@@ -1,6 +0,0 @@
-const worker: Worker = self as any
-
-worker.addEventListener('message', (event) => {
-  console.log('Worker Received:', event.data)
-  worker.postMessage({ result: event.data })
-})
